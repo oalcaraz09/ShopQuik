73f97c7bbf47c7487f8a65037751b003b63ebce4
adgeorge <adgeorge@cpp.edu>
2017-02-11 18:37:13 -0800

    added signin

--- shopquik/login/views.py
+++ shopquik/login/views.py
@@ -6,3 +6,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def signin(request):
+    return render(request, "shopquik/signin.html/")
