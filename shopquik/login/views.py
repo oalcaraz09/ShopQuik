f7bb25007dbdb11fdbae4f7fee3f3baac1a53209
Ashwin Rao <ashwincr@usc.edu>
2017-02-11 19:48:04 -0800

    Revert previous commit

--- shopquik/login/views.py
+++ shopquik/login/views.py
@@ -6,5 +6,3 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def signin(request):
-    return render(request, "shopquik/signin.html/")
